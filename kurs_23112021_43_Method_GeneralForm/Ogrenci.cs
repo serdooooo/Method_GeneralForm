ef5e00d696b19c79edc94fa2fe4fabc2b9052426
karakurtserdar19@gmail.com <karakurtserdar19@gmail.com>
2021-11-23 16:40:53 +0300

    Add project files.

---
+++ kurs_23112021_43_Method_GeneralForm/Ogrenci.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kurs_23112021_43_Method_GeneralForm
+{
+    public class Ogrenci
+    {
+        //Method oluşturma:
+        //(public,private, protected)+(void,string,int)+(method adı(parametreler)){yazılacak kodlar }
+        public void selamlaOgrenci()
+        {
+            Console.WriteLine("Merhaba Öğrenci");
+            
+        }
+        private void ogrenciMethod1(string ogrenciAdi,string ogrenciSoyadi)
+        {
+            Console.WriteLine("Öğrenci bilgileri: {0} {1}",ogrenciAdi,ogrenciSoyadi);
+        }
+        public void ogrenciMethod2()
+        {
+            ogrenciMethod1("Serdar", "Karakurt");
+        }
+        public string adGetir()
+        {
+            return " ";
+        }
+    }
+}
---
+++ kurs_23112021_43_Method_GeneralForm/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kurs_23112021_43_Method_GeneralForm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Ogrenci o1 = new Ogrenci();
+
+            
+            o1.ogrenciMethod2();
+            o1.selamlaOgrenci();
+
+            Ogrenci o2 = new Ogrenci();
+            o2.ogrenciMethod2();
+            o2.selamlaOgrenci();
+
+            Console.ReadLine();
+        }
+        static void Selamla()
+        {
+            Console.WriteLine("Merhaba");
+        }
+    }
+}
